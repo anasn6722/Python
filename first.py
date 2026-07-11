6066527171705b960be8b9c3ca57b2c0a421dca5
Anas Nasir <anasn6722@gmail.com>
2026-07-11 18:20:15 +0500

    added new functions in file

--- first.py
+++ first.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+def add2num(a,b):
+    sum=a+b
+    print("Sum is:",sum)
+
+def avg2num(a,b):
+    avg=(a+b)/2
+    print("Average is:",avg)
+
+def sqr_num():
+    num1=int(input("Enter a number:"))
+    num1**=2
+    print("Square is:",num1)
+
+add2num(5,6)
+avg2num(5,7)
+sqr_num()
